6e712640de13985dd7331bf103ee9d8380487b7f
Susanto <nerdyspook007@gmail.com>
2021-08-17 20:43:54 +0530

    style: added initial html css js files

---
+++ js/main.js
@@ -0,0 +1,5 @@
+const initialPrice = document.querySelector("#initial");
+const currentPrice = document.querySelector("#current");
+const quantity = document.querySelector("#quantity");
+const checkButton = document.querySelector("#btn-check");
+const output = document.querySelector("#output");
